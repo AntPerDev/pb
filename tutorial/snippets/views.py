a56be819eaeaeb81f68ec3a5c5d1799809a1f2fc
AntPerDev <perezterreroantonio@gmail.com>
2020-12-21 14:14:55 +0100

    Creating an endpoint for the root of our API

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -6,6 +6,10 @@ from .serializers import UserSerializer
 from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -31,3 +35,12 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
